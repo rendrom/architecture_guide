628b0d7776c7cabcb42a47f7c62471a3d0ea934c
Artemiy <rendrom@gmail.com>
2024-11-16 20:57:09 +0200

    wip: layer visibility change strictly

--- src/panels/legendTree.tsx
+++ src/panels/legendTree.tsx
@@ -1,5 +1,5 @@
 import { Tree } from 'antd';
-import { useCallback, useMemo, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
 import type { NgwWebmapItem } from '@nextgis/ngw-kit';
 import type { TreeProps } from 'antd';
@@ -8,6 +8,7 @@ type TreeItem = {
   title: string;
   key: string;
   children?: TreeItem[];
+  layer?: NgwWebmapItem;
 };
 
 export const LegendTree = ({ layer }: { layer: NgwWebmapItem }) => {
@@ -15,22 +16,30 @@ export const LegendTree = ({ layer }: { layer: NgwWebmapItem }) => {
   const [expandedKeys, setExpandedKeys] = useState<React.Key[]>([]);
   const [autoExpandParent, setAutoExpandParent] = useState<boolean>(true);
   const onCheck: TreeProps['onCheck'] = (checkedKeysValue) => {
-    setCheckedKeys(checkedKeysValue as React.Key[]);
+    if (Array.isArray(checkedKeysValue)) {
+      setCheckedKeys(checkedKeysValue);
+    } else {
+      setCheckedKeys(checkedKeysValue.checked);
+    }
   };
   const onExpand: TreeProps['onExpand'] = (expandedKeysValue) => {
     setExpandedKeys(expandedKeysValue);
     setAutoExpandParent(false);
   };
 
+  const treeItemsRef = useRef<TreeItem[]>([]);
+
   const counter = useRef(1);
 
   const getTree = useCallback((layer: NgwWebmapItem) => {
-    const key = `layer ${counter.current++}`;
-
+    const key = layer.layer
+      ? `layer-${counter.current++}-${layer.layer.id}`
+      : `counter-${counter.current++}`;
     // tree item obj
     const treeItem: TreeItem = {
       title: layer.item?.display_name ?? `layer ${counter}`,
       key,
+      layer,
     };
     // item children array
     const children: TreeItem[] = [];
@@ -46,6 +55,7 @@ export const LegendTree = ({ layer }: { layer: NgwWebmapItem }) => {
       });
       treeItem.children = children;
     }
+    treeItemsRef.current.push(treeItem);
     return treeItem;
   }, []);
 
@@ -57,12 +67,20 @@ export const LegendTree = ({ layer }: { layer: NgwWebmapItem }) => {
     }
   }, [getTree, layer]);
 
-  // console.log(getTree(layer, 1));
+  useEffect(() => {
+    for (const treeItem of treeItemsRef.current) {
+      treeItem.layer?.properties.set(
+        'visibility',
+        checkedKeys.includes(treeItem.key),
+      );
+    }
+  }, [checkedKeys]);
 
   return (
     <div>
       <Tree
         checkable
+        checkStrictly
         checkedKeys={checkedKeys}
         onCheck={onCheck}
         expandedKeys={expandedKeys}
